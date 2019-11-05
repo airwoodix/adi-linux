analogdevicesinc/linux

staging: iio: ad5933: Add a regulator for vref
In order to add full device tree support, add the option of specifying
the vref using a regulator too.

Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

drivers/staging/iio/impedance-analyzer/ad5933.c
@@ -97,6 +97,7 @@ struct ad5933_platform_data {
 struct ad5933_state {
 	struct i2c_client		*client;
 	struct regulator		*reg;
+	struct regulator		*vref;
 	struct delayed_work		work;
 	unsigned long			mclk_hz;
 	unsigned char			ctrl_hb;
@@ -734,6 +735,14 @@ static int ad5933_probe(struct i2c_client *client,
 		voltage_uv = regulator_get_voltage(st->reg);
 	}
 
+	st->reg = devm_regulator_get(&client->dev, "vref");
+	if (!IS_ERR(st->vref)) {
+		ret = regulator_enable(st->vref);
+		if (ret)
+			goto error_disable_reg;
+		pdata->vref_mv = regulator_get_voltage(st->vref) / 1000;
+	}
+
 	if (voltage_uv)
 		st->vref_mv = voltage_uv / 1000;
 	else
@@ -760,7 +769,7 @@ static int ad5933_probe(struct i2c_client *client,
 
 	ret = ad5933_register_ring_funcs_and_init(indio_dev);
 	if (ret)
-		goto error_disable_reg;
+		goto error_disable_vref;
 
 	ret = ad5933_setup(st);
 	if (ret)
@@ -774,6 +783,9 @@ static int ad5933_probe(struct i2c_client *client,
 
 error_unreg_ring:
 	iio_kfifo_free(indio_dev->buffer);
+error_disable_vref:
+	if (!IS_ERR(st->vref))
+		regulator_disable(st->vref);
 error_disable_reg:
 	if (!IS_ERR(st->reg))
 		regulator_disable(st->reg);
@@ -790,6 +802,8 @@ static int ad5933_remove(struct i2c_client *client)
 	iio_kfifo_free(indio_dev->buffer);
 	if (!IS_ERR(st->reg))
 		regulator_disable(st->reg);
+	if (!IS_ERR(st->vref))
+		regulator_disable(st->vref);
 
 	return 0;
 }

airwoodix/adi-linux

(cherry picked from commit ecee88f71af786a7da9702f2ee060a24e3a3e2b4)

drivers/staging/iio/impedance-analyzer/ad5933.c
@@ -85,6 +85,7 @@
 struct ad5933_state {
 	struct i2c_client		*client;
 	struct regulator		*reg;
+	struct regulator		*vref;
 	struct clk			*mclk;
 	struct delayed_work		work;
 	struct mutex			lock; /* Protect sensor state */
@@ -708,6 +709,19 @@ static int ad5933_probe(struct i2c_client *client,
 	if (ret < 0)
 		goto error_disable_reg;
 
+	st->reg = devm_regulator_get(&client->dev, "vref");
+	if (!IS_ERR(st->vref)) {
+		ret = regulator_enable(st->vref);
+		if (ret)
+			goto error_disable_reg;
+		st->vref_mv = regulator_get_voltage(st->vref) / 1000;
+	}
+
+	if (voltage_uv)
+		st->vref_mv = voltage_uv / 1000;
+	else
+		st->vref_mv = pdata->vref_mv;
+
 	st->vref_mv = ret / 1000;
 
 	st->mclk = devm_clk_get(&client->dev, "mclk");
@@ -760,6 +774,8 @@ error_unreg_ring:
 	iio_kfifo_free(indio_dev->buffer);
 error_disable_mclk:
 	clk_disable_unprepare(st->mclk);
+error_disable_vref:
+	regulator_disable(st->vref);
 error_disable_reg:
 	regulator_disable(st->reg);
 
@@ -775,6 +791,7 @@ static int ad5933_remove(struct i2c_client *client)
 	iio_kfifo_free(indio_dev->buffer);
 	regulator_disable(st->reg);
 	clk_disable_unprepare(st->mclk);
+	regulator_disable(st->vref);
 
 	return 0;
 }