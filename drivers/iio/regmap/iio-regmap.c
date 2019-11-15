941fcbc3ec0fd503296ed7f0d1f3d48cc76a54bc
Alexandru Tachici <alexandru.tachici@analog.com>
2019-11-15 18:27:01 +0200

    iio: iio-regmap: firmware access
    Added function for reading the firmware.
    The firmware name should be specified in the device tree
    as property "firmware".
    
    Signed-off-by: Alexandru Tachici <alexandru.tachici@analog.com>

--- drivers/iio/regmap/iio-regmap.c
+++ drivers/iio/regmap/iio-regmap.c
@@ -6,6 +6,7 @@
  */
 
 #include <linux/iio/iio.h>
+#include <linux/firmware.h>
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/regmap.h>
@@ -45,11 +46,54 @@ int iio_regmap_read_config(struct device *dev, struct regmap_config *regmap_cfg)
 }
 EXPORT_SYMBOL_GPL(iio_regmap_read_config);
 
+/* Retrieve from device node the firmware name then
+ * read from firmware register operations,
+ * allocate a copy of the firmware data
+ * and return the start address of register operations within
+ * the firmware.
+ */
+static const char *read_firmware(struct device *dev)
+{
+	char *reg_ops;
+	int ret;
+	const char *firmware_name;
+	const char *data;
+	const struct firmware *firmware;
+
+	ret = device_property_read_string(dev, "firmware", &firmware_name);
+	if (ret < 0) {
+		dev_err(dev, "Firmware name property read failed!\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	ret = request_firmware(&firmware, firmware_name, dev);
+	if (ret < 0) {
+		dev_err(dev, "request_firmware failed!\n");
+		return ERR_PTR(-EINVAL);
+	}
+
+	reg_ops = devm_kzalloc(dev, (firmware->size + 1) * sizeof(char),
+			       GFP_KERNEL);
+	if (!reg_ops)
+		return ERR_PTR(-ENOMEM);
+
+	data = firmware->data;
+	if (!data) {
+		dev_err(dev, "Firmware data not loaded.");
+		return ERR_PTR(-EINVAL);
+	}
+	memcpy(reg_ops, data, (firmware->size) * sizeof(char));
+	release_firmware(firmware);
+
+	return reg_ops;
+}
+
 int iio_regmap_probe(struct device *dev, struct regmap *regmap,
 		     const char *name)
 {
 	struct iio_dev *indio_dev;
 	struct iio_regmap *st;
+	const char *register_ops;
 	int ret;
 
 	indio_dev = devm_iio_device_alloc(dev, sizeof(*st));
@@ -70,6 +114,12 @@ int iio_regmap_probe(struct device *dev, struct regmap *regmap,
 	ret = devm_iio_device_register(dev, indio_dev);
 	if (ret < 0)
 		dev_err(&indio_dev->dev, "iio-regmap device register failed\n");
+
+	register_ops = read_firmware(dev);
+	if (IS_ERR(register_ops)) {
+		dev_err(dev, "read_firmware failed!\n");
+		return PTR_ERR(register_ops);
+	}
 
 	return ret;
 }
